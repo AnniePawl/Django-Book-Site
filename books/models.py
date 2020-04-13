6d9e188df778898a28570da766fa647536f931e0
Anna Pawl <annampawl@gmail.com>
2020-04-12 22:53:22 -0400

    books and author models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    num_pages = models.IntegerField()
+    date_published = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
