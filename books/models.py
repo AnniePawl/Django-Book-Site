d9edc02e7c09eeb739aca79a22c56b739956ba6e
Anna Pawl <annampawl@gmail.com>
2020-04-12 23:32:25 -0400

    tag model

--- books/models.py
+++ books/models.py
@@ -3,10 +3,20 @@ from django.db import models
 # Create your models here.
 
 
-class Books(models.Model):
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Book(models.Model):
     title = models.CharField(max_length=100)
     num_pages = models.IntegerField()
     date_published = models.DateField()
+    tags = models.ManyToManyField(Tag)
+    author = models.ForeignKey(
+        'Author',
+        on_delete=models.CASCADE,
+        null=True,
+    )
 
     def __str__(self):
         return self.title
@@ -18,3 +28,4 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
