04c801fd084d99c1b7053338656a3b0f478c475d
Anna Pawl <annampawl@gmail.com>
2020-04-14 00:24:29 -0400

    fixed detail view

--- books/models.py
+++ books/models.py
@@ -1,17 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=100)
-
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     num_pages = models.IntegerField()
     date_published = models.DateField()
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField('Tag')
     author = models.ForeignKey(
         'Author',
         on_delete=models.CASCADE,
@@ -22,10 +16,16 @@ class Book(models.Model):
         return self.title
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
     birth_date = models.DateField()
 
     def __str__(self):
         return self.name
-
--- books/views.py
+++ books/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 from .models import Book
 
 
-# Create your views here.
-
-
+# Home View
 def home(request):
     context = {
         'books': Book.objects.all()
     }
-    return render(request, 'home.html', context)
+    return render(request, 'books/home.html', context)
+
+
+# Detail View
+def detail(request, book_id):
+    context = {
+        'book': Book.objects.get(id=book_id)
+    }
+    return render(request, 'books/detail.html', context)
